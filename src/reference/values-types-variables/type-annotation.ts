68fda5be6196ebae14f815979c166aa4d04c4ef9
blue32a <blue32a.jp@gmail.com>
2022-08-11 14:44:30 +0900

    変数宣言の型注釈 (type annotation)

---
+++ src/reference/values-types-variables/type-annotation.ts
@@ -0,0 +1,11 @@
+/* 変数宣言の型注釈 (type annotation) */
+(() => {
+  const num: number = 123;
+  console.log(num);
+
+  const str: string = 'hello';
+  console.log(str);
+
+  const arr: number[] = [1, 2, 3];
+  console.log(arr);
+})();
